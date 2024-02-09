bd9d20dcb784377c502b559f4965b32852821033
HRS148 <130727632+HRS148@users.noreply.github.com>
2024-02-09 12:30:59 +0000

    commited

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+// Number
+// BigInt
+// String
+// Boolean
+// Symbol->unique
+// null-> standalone value
+// undefined
+// Object
+
+let a = 20;
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
+
+console.log(typeof "hrithik");//string
+console.log(typeof a);//number
