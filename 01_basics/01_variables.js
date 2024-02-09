012874cc590f28a715638c74e335d36a8fa83d31
HRS148 <130727632+HRS148@users.noreply.github.com>
2024-02-09 12:24:52 +0000

    commited

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,38 @@
+// const accountId = 121344;
+// let accountEmail = "hrithiksharma1655@gmail.com";
+// var accountaddress="delhi";
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountaddress);
+// console.log(typeof(accountEmail));
+
+let x = 10;
+{
+  let x = 2;
+  console.log(x);
+}
+console.log(x);
+
+let y = 10;
+{
+//   console.log(y);
+  let y = 2;
+  console.log(y);
+}
+
+var z = 10;
+{
+  console.log(z);
+  var z = 2;
+  console.log(z);
+}
+console.log(z);
+
+let a = 10;
+{
+  console.log(a);
+  let a = 2;
+  console.log(a);
+}
+console.log(a);
